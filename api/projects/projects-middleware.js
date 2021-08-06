03d71c1d12b3556fe92270ec2226cb36048ebae4
Matt Phillips <mattcphillips17@gmail.com>
2021-08-06 03:28:14 -0700

    projects router and middleware complete

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,2 +1,35 @@
 // add middlewares here related to projects
 const Projects = require('./projects-model')
+
+async function validateProjID(req, res, next) {
+  try {
+    const { id } = req.params
+    const project = await Projects.get(id)
+    if (project) {
+      req.project = project
+      next()
+    } else {
+      res.status(404).json({
+        message: 'Project ID Not Found'
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
+}
+
+function validateProjBody(req, res, next) {
+  const { name, description } = req.body
+  if (!name || !description) {
+    res.status(400).json({
+      message: 'name and description required'
+    })
+  } else {
+    next()
+  }
+} 
+
+module.exports = {
+  validateProjID,
+  validateProjBody
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,6 +3,11 @@ const express = require('express')
 
 const Projects = require('./projects-model')
 
+const {
+  validateProjID,
+  validateProjBody
+} = require('./projects-middleware')
+
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
@@ -15,6 +20,42 @@ router.get('/', (req, res, next) => {
         message: err.message
       })
     })
+})
+
+router.get('/:id', validateProjID, (req, res, next) => {
+  res.status(200).json(req.project)
+})
+
+router.post('/', validateProjBody, (req, res, next) => {
+  Projects.insert(req.body)
+    .then(project => {
+      res.status(201).json(project)
+    })
+    .catch(next)
+})
+
+router.put('/:id', validateProjID, validateProjBody, (req, res, next) => {
+  Projects.update(req.params.id, req.body)
+    .then(updated => {
+      res.status(200).json(updated)
+    })
+    .catch(next)
+})
+
+router.delete('/:id', validateProjID, (req, res, next) => {
+  Projects.remove(req.params.id)
+    .then(() => {
+      res.status(200).json({ message: 'project successfully deleted'})
+    })
+    .catch(next)
+})
+
+router.get('/:id/actions', validateProjID, (req, res, next) => {
+  Projects.getProjectActions(req.params.id)
+    .then(actions => {
+      res.status(200).json(actions)
+    })
+    .catch(next)
 })
 
 module.exports = router
